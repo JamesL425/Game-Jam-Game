e9cd65d193f13dcb0695275e502968b2efe17438
JamesL425 <jamesleung425@gmail.com>
2025-03-24 19:33:56 +0000

    Update parcel

--- scenes/game.gd
+++ scenes/game.gd
@@ -7,3 +7,9 @@ signal parcel_resolved
 func _on_parcel_resolved() -> void:
 	var parcel = parcel_scene.instantiate()
 	parcel.position = $ParcelStartPosition.position
+
+func new_game():
+	pass
+
+func game_over():
+	pass	
