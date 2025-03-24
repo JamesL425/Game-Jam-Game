20b0f34e362579d4890e53845c4235c0a7ed1bdd
Matt Younger <myounger009@gmail.com>
2025-03-24 15:46:50 +0000

    temporary assets + blah blah blah

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,1 +1,12 @@
 extends Control
+
+
+func _on_play_button_pressed() -> void:
+	# TODO: Load before?
+	get_tree().change_scene_to_file("res://scenes/game.tscn");
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit();
+	pass # Replace with function body.
