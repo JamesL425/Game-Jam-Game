16ae035f3bd44a621815e0ebf40b9c986fe824c7
Matt Younger <myounger009@gmail.com>
2025-03-24 17:32:49 +0000

    Generate address (simple)

--- scripts/locations/load_locations.gd
+++ scripts/locations/load_locations.gd
@@ -1,8 +1,25 @@
 extends Node
 
-class_name World;
-
 @export var locations_json: JSON;
 
+var locations: Variant;
+
+@onready var rng: RandomNumberGenerator = RandomNumberGenerator.new();
+
 func _ready() -> void:
-	pass
+	locations = locations_json.get_data()
+	rng.randomize()
+	print(create_address())
+
+
+# TODO: weighted random based on population
+func create_address() -> String:
+	var country_index = rng.randi_range(0, locations.size()-1)
+	var country = locations[country_index]
+	var city_index = rng.randi_range(0, country.cities.size() - 1)
+	var city = country.cities[city_index]
+	var street_index = rng.randi_range(0, city.streets.size() - 1)
+	var street = city.streets[street_index]
+	var house = rng.randi_range(1, street.length);
+	var out: String = str(house) + " " + street.name + "\n" + city.name + "\n" + country.name;
+	return out
