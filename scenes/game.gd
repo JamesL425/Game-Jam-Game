fa72e3789dd445f217fe3da3b999415eab36bfd9
JamesL425 <jamesleung425@gmail.com>
2025-03-25 09:57:42 +0000

    Parcel Start Position

--- scenes/game.gd
+++ scenes/game.gd
@@ -5,11 +5,14 @@ signal parcel_resolved
 @export var parcel_scene: PackedScene
 
 func _on_parcel_resolved() -> void:
+	get_tree().call_group("parcel_group", "queue_free")
 	var parcel = parcel_scene.instantiate()
 	parcel.position = $ParcelStartPosition.position
+	add_child(parcel)
 
 func new_game():
 	var parcel = parcel_scene.instantiate()
+	parcel.position = $ParcelStartPosition.position
 	add_child(parcel)
 	
 func game_over():
--- scenes/parcel.gd
+++ scenes/parcel.gd
@@ -1,1 +1,8 @@
 extends RigidBody2D
+
+func on_shake(delta):
+	# add animation
+	postion += 
+	
+	# add sound
+	pass
